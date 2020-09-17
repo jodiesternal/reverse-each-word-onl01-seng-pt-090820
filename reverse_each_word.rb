fbe5b97f55ea19efe577f22bdfb19dd4d6790790
global-listener-7039 <jodiesternal@yahoo.com>
2020-09-17 01:23:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,14 +1,7 @@
 require "pry"
 def reverse_each_word(sentence)
-  #  binding.pry
-  sentence.split
-    sentence.each do |sentence|
-      s
-
-
+  reversed = sentence.split(" ").collect do |word|
+  word.reverse
 end
-
-
-
-
-# " now's  the time".split
+  reversed.join(" ")
+end
